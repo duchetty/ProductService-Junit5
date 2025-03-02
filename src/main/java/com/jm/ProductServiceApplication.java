348368fbf6fb835f895e0033a64145f6464ab9d1
duchetty <147183367+duchetty@users.noreply.github.com>
2025-03-02 11:27:35 +0530

    Update ProductServiceApplication.java

--- src/main/java/com/jm/ProductServiceApplication.java
+++ src/main/java/com/jm/ProductServiceApplication.java
@@ -9,6 +9,7 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 		System.out.println("Junit5&Mockito Examples and Test cases are passed");
+		System.out.println("All Test cases are passed");
 	}
 
 }
